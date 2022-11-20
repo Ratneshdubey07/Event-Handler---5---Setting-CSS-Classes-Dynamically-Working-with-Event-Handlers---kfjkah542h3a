e75a4832f1c23ecdd0bdf59441f8adfd9342d099
Ratneshdubey07 <111305768+Ratneshdubey07@users.noreply.github.com>
2022-11-20 21:08:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,22 @@ import React from 'react'
 import '../styles/App.css';
 const App = () => {
 
-
+  let timer;
+  const handleDoubleClick = (e)=> {
+    if(e.detail === 1){
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        console.log('I was not double clicked');
+      }, 500);
+    }
+    else {
+      clearTimeout(timer);
+        console.log('I was double clicked');
+    }
+  }
   return (
     <div id="main">
-      <form>
-        <label htmlFor='name'>Name</label>
-        <input id="name" type={"text"} />
-
-        <br />
-        <br />
-        <button type='submit'>Submit</button>
-      </form>
+      <button id="dblclick-btn" onClick={handleDoubleClick}>Double click me</button>
     </div>
   )
 }
